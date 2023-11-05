cf898bbd7a73b771235ab985c503a0f773880b15
Yefferson Olivos <76671366+Yeronn@users.noreply.github.com>
2023-11-05 15:09:09 -0500

    Proyecto 3 Mouse Follower

---
+++ projects/03-Mouse-follower/src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+    console.log('efecto', enabled)
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: '50%',
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'}</button>
+    </main>
+  )
+}
+
+export default App
